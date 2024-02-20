23649966462247d9efa4bc46a9fb487de535713b
The-real-joe <j.boucher690@gmail.com>
2024-02-20 11:33:37 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+const port = 8000
+
+server.listen(port, () => {
+    console.log(`api listening on ${port}`)
+})
+
